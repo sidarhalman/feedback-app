ae07e243b4a9e8693bba757bf0ace0b72ef5f3b5
sidarhalman <sidarhalman93@gmail.com>
2022-07-28 16:23:46 +0200

    add components

--- feedback-frontend/src/App.js
+++ feedback-frontend/src/App.js
@@ -1,6 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
     return (
-        <h1>Hello from App component</h1>
+        <div className="App">
+            <Header />
+            <p>This is my first react app</p>
+        </div>
     );
 }
 
