c5aeba1febbfd4dd74557268fd77602398643075
sidarhalman <sidarhalman93@gmail.com>
2022-07-28 17:01:31 +0200

    add style for FeedbackItem

--- feedback-frontend/src/App.js
+++ feedback-frontend/src/App.js
@@ -1,10 +1,11 @@
 import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
 
 function App() {
     return (
         <div className="App">
             <Header />
-            <p>This is my first react app</p>
+            <FeedbackItem />
         </div>
     );
 }
--- feedback-frontend/src/components/FeedbackItem.jsx
+++ feedback-frontend/src/components/FeedbackItem.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import '../styles/FeedbackItem.css'
 
 function FeedbackItem() {
   return (
-    <div>FeedbackItem</div>
+    <div className='card'>
+        <div className="num-display">10</div>
+        <div className="text-display">Lorem ipsum dolor sit amet.</div>
+    </div>
   )
 }
 
